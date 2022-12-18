8e289eaf370c22c8e4b5c9b438905677fb8b8da5
uzduygu <uzduygu21@gmail.com>
2022-12-17 17:47:49 -0800

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComponentsComponent } from './components/components.component';
+import { LandingComponent } from './examples/landing/landing.component';
+import { NucleoiconsComponent } from './components/nucleoicons/nucleoicons.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '',      component: ComponentsComponent, pathMatch: 'full' },
+  { path: 'landing',          component: LandingComponent },
+  { path: 'nucleoicons',      component: NucleoiconsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject, Renderer2, ElementRef, ViewChild } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter } from 'rxjs';
+import { DOCUMENT } from '@angular/common';
+import { LocationStrategy, PlatformLocation, Location } from '@angular/common';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'servet-wealth-management';
+  private _router: Subscription = new Subscription;
+  @ViewChild(NavbarComponent) navbar!: NavbarComponent;
+
+
+  constructor( private renderer : Renderer2, private router: Router, @Inject(DOCUMENT,) private document: any, private element : ElementRef, public location: Location) {}
+
+  ngOnInit() {
+    var navbar : HTMLElement = this.element.nativeElement.children[0].children[0];
+    // this._router = this.router.events.filter(event => event instanceof NavigationEnd).subscribe((event: NavigationEnd) => {
+    //     if (window.outerWidth > 991) {
+    //         window.document.children[0].scrollTop = 0;
+    //     }else{
+    //         window.document.activeElement.scrollTop = 0;
+    //     }
+    //     this.navbar.sidebarClose();
+    // });
+    this.renderer.listen('window', 'scroll', (event) => {
+        const number = window.scrollY;
+        if (number > 150 || window.pageYOffset > 150) {
+            // add logic
+            navbar.classList.remove('navbar-transparent');
+        } else {
+            // remove logic
+            navbar.classList.add('navbar-transparent');
+        }
+    });
+    var ua = window.navigator.userAgent;
+    var trident = ua.indexOf('Trident/');
+    if (trident > 0) {
+        // IE 11 => return version number
+        var rv = ua.indexOf('rv:');
+        var version = parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
+
+}
+removeFooter() {
+    var titlee = this.location.prepareExternalUrl(this.location.path());
+    titlee = titlee.slice( 1 );
+    if(titlee === 'signup' || titlee === 'nucleoicons'){
+        return false;
+    }
+    else {
+        return true;
+    }
+}
 }
