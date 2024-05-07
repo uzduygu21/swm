a4e9cd59487144bc2d3d79e3415b047a2aaf2b99
uzduygu <uzduygu21@gmail.com>
2024-05-07 15:31:11 -0400

    blog 10 updated

--- src/app/examples/blog/bold-keyword.pipe.ts
+++ src/app/examples/blog/bold-keyword.pipe.ts
@@ -1,19 +1,26 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Pipe({
   name: 'boldKeyword'
 })
 export class BoldKeywordPipe implements PipeTransform {
 
-  transform(value: string): string {
+  constructor(private sanitizer: DomSanitizer) {}
+
+  transform(value: string): SafeHtml {
     const splitIndex = value.indexOf(':');
     
     if (splitIndex !== -1) {
       const keyword = value.substring(0, splitIndex);
       const restOfText = value.substring(splitIndex + 1);
       
-      return `<strong>${keyword}</strong>: ${restOfText}`;
+      const boldKeyword = `<span style="font-weight: bold;">${keyword}</span>`;
+      const transformedValue = boldKeyword + ': ' + restOfText;
+      
+      return this.sanitizer.bypassSecurityTrustHtml(transformedValue);
     }
+
     return value;
   }
 
